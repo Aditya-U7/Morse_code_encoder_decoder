d338d3a04f161df5d560b5062b147f085d9b6cf7
Aditya Upadhye <66748266+Aditya-U7@users.noreply.github.com>
2023-06-30 00:04:09 +0530

    Add files via upload

--- morse_encoder_decoder.py
+++ morse_encoder_decoder.py
@@ -1,62 +1,73 @@
+""" 
+
+Author: Aditya Upadhye
+
+A Python program for encoding of and decoding from Morse code.
+
+"""
+
+
+
+
 morse_to_alpha = {
-    ".-": "A",
-    "-...": "B",
-    "-.-.": "C",
-    "-..": "D",
-    ".": "E",
-    "..-.": "F",
-    "--.": "G",
-    "....": "H",
-    "..": "I",
-    ".---": "J",
-    "-.-": "K",
-    ".-..": "L",
-    "--": "M",
-    "-.": "N",
-    "---": "O",
-    ".--.": "P",
-    "--.-": "Q",
-    ".-.": "R",
-    "...": "S",
-    "-": "T",
-    "..-": "U",
-    "...-": "V",
-    ".--": "W",
-    "-..-": "X",
-    "-.--": "Y",
-    "--..": "Z",
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
 }
 
 
 morse_to_digit = {
-    ".----": "1",
-    "..---": "2",
-    "...--": "3",
-    "....-": "4",
-    ".....": "5",
-    "-....": "6",
-    "--...": "7",
-    "---..": "8",
-    "----.": "9",
-    "-----": "0",
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 }
 
 
 morse_to_punctuation = {
-    ".-.-.-": ".",
-    "--..--": ",",
-    "..--..": "?",
-    "-.-.-.": ";",
-    "---...": ":",
-    "-....-": "-",
-    ".----.": "'",
-    ".-..-.": '"',
-    "-...-": "=",
-    ".-.-.": "+",
-    "-.--.": "(",
-    "-.--.-": ")",
-    ".--.-.": "@",
-    "-..-": "X",
+    '.-.-.-': '.',
+    '--..--': ',',
+    '..--..': '?',
+    '-.-.-.': ';',
+    '---...': ':',
+    '-....-': '-',
+    '.----.': ''',
+    '.-..-.': ''',
+    '-...-': '=',
+    '.-.-.': '+',
+    '-.--.': '(',
+    '-.--.-': ')',
+    '.--.-.': '@',
+    '-..-': 'X',
 }
 
 
@@ -69,8 +80,8 @@ punctuation_to_morse = {value: key for key, value in morse_to_punctuation.items(
 
 def morse_code_decoder(morse):
 
-    morse = morse.split("/")
-    decoded_morse = ""
+    morse = morse.split('/')
+    decoded_morse = ''
 
     for each in morse:
 
@@ -90,16 +101,16 @@ def morse_code_decoder(morse):
 
                 decoded_morse += morse_to_alpha[word]
 
-        decoded_morse += " "
+        decoded_morse += ' '
 
     return decoded_morse
 
 
 def morse_code_encoder(text):
 
-    text = text.split(" ")
+    text = text.split(' ')
 
-    morse_code = ""
+    morse_code = ''
 
     for each_word in text:
 
@@ -117,32 +128,32 @@ def morse_code_encoder(text):
 
                 morse_code += punctuation_to_morse[character]
 
-            morse_code += " "
+            morse_code += ' '
 
-        morse_code += "/"
+        morse_code += '/'
 
     return morse_code
 
 
 def check_user_choice(user_choice):
 
-    while 1:
+    while True:
 
-        if user_choice == "mct" or user_choice == "tmc":
+        if user_choice == 'mct' or user_choice == 'tmc':
 
             return user_choice
 
         else:
 
-            user_choice = input("Invalid option. Please enter your choice again.\n")
+            user_choice = input('Invalid option. Please enter your choice again.\n')
 
 
 def read_user_input():
 
     user_choice = input(
-        "\nProvide the conversion scheme:\n\n"
-        "For Morse code to text : mct \n"
-        "For text to Morse code : tmc\n\n"
+        '\nProvide the conversion scheme:\n\n'
+        'For Morse code to text : mct \n'
+        'For text to Morse code : tmc\n\n'
     )
 
     user_choice = check_user_choice(user_choice)
@@ -153,23 +164,23 @@ def read_user_input():
 """ This is where the program starts. """
 
 
-conversion_scheme = {"mct": "Morse code to text", "tmc": "Text to Morse code"}
+conversion_scheme = {'mct': 'Morse code to text', 'tmc': 'Text to Morse code'}
 
 user_selected_scheme = read_user_input()
 
-if user_selected_scheme == "mct":
+if user_selected_scheme == 'mct':
 
-    morse_code = input("\nEnter the Morse code:\n\n")
+    morse_code = input('\nEnter the Morse code:\n\n')
 
     answer = morse_code_decoder(morse_code)
 
 else:
 
-    plain_text = input("\nEnter the text:\n\n")
+    plain_text = input('\nEnter the text:\n\n')
 
     answer = morse_code_encoder(plain_text.upper())
 
-print("\nFrom ", conversion_scheme[user_selected_scheme], ":\n", sep="")
+print('\nFrom ', conversion_scheme[user_selected_scheme], ':\n', sep='')
 
 print(answer)
 
